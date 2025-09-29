694dd1c1f749022cf6e7bd6003d7f83247d1c0ff
Thasi <ducthai132003@gmail.com>
2025-09-29 11:49:12 +0700

    vcd

---
+++ resources/js/user.js
@@ -0,0 +1,32 @@
+document.addEventListener('click', async (e) => {
+  if (e.target.matches('.btn-block')) {
+    const id = e.target.dataset.id
+    const name = e.target.dataset.name
+
+    const res = await fetch('/block', {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ senderId: id, sender: name }),
+    })
+
+    alert(`Đã chặn người dùng: ${name}`)
+    window.location.reload()
+  }
+})
+
+
+document.addEventListener('click', async (e) => {
+  if (e.target.matches('.btn-unblock')) {
+    const id = e.target.dataset.id
+    const name = e.target.dataset.name
+
+    const res = await fetch('/unblock', {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ senderId: id, sender: name }),
+    })
+
+    alert(`Đã bỏ chặn người dùng: ${name}`)
+    window.location.reload()
+  }
+})
