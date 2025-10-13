378c91d1350c5d7a1e576fa8bdb4b7b10410f001
Thasi <ducthai132003@gmail.com>
2025-10-13 12:22:14 +0700

    đm

--- start/env.ts
+++ start/env.ts
@@ -39,5 +39,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   REDIS_HOST: Env.schema.string({ format: 'host' }),
   REDIS_PORT: Env.schema.number(),
   REDIS_PASSWORD: Env.schema.string.optional(),
-  JWT_SECRET: Env.schema.string(),
 })
