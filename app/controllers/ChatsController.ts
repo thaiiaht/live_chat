a0e077f374ded7d1b99ed6e988b343735ee48df0
Thasi <ducthai132003@gmail.com>
2025-10-23 15:58:28 +0700

    test

--- app/controllers/ChatsController.ts
+++ app/controllers/ChatsController.ts
@@ -26,14 +26,14 @@ export default class ChatsController {
           role: string
         }
         console.log(roomId)
-        await enqueueUserCreation({
-        id: user.id,
-        email: user.email,
-        fullName: user.fullName,
-        roomId,
-        partner: mainName,
-        role: user.role,
-      })
+      //   await enqueueUserCreation({
+      //   id: user.id,
+      //   email: user.email,
+      //   fullName: user.fullName,
+      //   roomId,
+      //   partner: mainName,
+      //   role: user.role,
+      // })
           transmit.broadcast(`join/${mainName}/${token}`, {
             event: 'user_joined',
             data: {
