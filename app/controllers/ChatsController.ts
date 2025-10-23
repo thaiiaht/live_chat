4a00604543bfd972b5715dbabe26af45a27eb5e3
Thasi <ducthai132003@gmail.com>
2025-10-23 15:15:32 +0700

    check

--- app/controllers/ChatsController.ts
+++ app/controllers/ChatsController.ts
@@ -49,7 +49,7 @@ export default class ChatsController {
             user,
           })
         } catch (error) {
-          return response.badRequest({ status: 'error', message: 'Invalid token' })
+          return response.badRequest({ status: 'error', message: 'Invalid token', error })
         }
       }
 
